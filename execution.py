59c624b8201c4cfeec7bb2265945fb4dd1bc8021
hibikikkk <kyou9712@gmail.com>
2018-11-20 17:39:46 +0900

    応用問題の自由に判別後の文字列変更とNGワードの複数登録とNGワードの変更できるようにした！
    判別後の文字列変更はクラスのイニシャルを増やして、管理できるようにした。
    NGワードの複数登録はリストで保存するようにした。
    NGワードの変更は変更できるようにするメソッドと保存されているNGワードリストを表示させるようにした。

--- execution.py
+++ execution.py
@@ -1,17 +1,30 @@
 from word_filter import WordFilter
 
 if __name__ == "__main__":
+    # 複数のNGワードを保存するためのリスト型変数
+    ng_word_list = []
+
+    # NGワードを入力するためのループ
+    while True:
+        ng_word_list.append(input("NGワードを入力してください >"))
+
+        if input("NGワードは他にありますか？(はい or いいえ) >") == "いいえ":
+            break
+
+    # 置き換える文字列の入力
     input_replace_word = input("置き換える文字列はなんですか？ > ")
 
+    # 置き換える文字列が文字列をWordFilterクラスに空文字でないなら保存する
     if input_replace_word != "":
-        my_filter = WordFilter("アーセナル", input_replace_word)
+        my_filter = WordFilter(ng_words=ng_word_list, replace_word=input_replace_word)
 
     if input_replace_word == "":
-        my_filter = WordFilter("アーセナル")
+        my_filter = WordFilter(ng_words=ng_word_list)
 
-    # print(my_filter.detect("昨日のアーセナルの試合アツかった！"))
-    #
-    # print(my_filter.detect("昨日のリバプールの試合アツかった！"))
+    # 動作が行えているか確認用ループ
+    while True:
+        print(my_filter.all_censor("昨日のアーセナルとリバプールの試合アツかった！"))
+        print(my_filter.all_censor("昨日のリバプールの試合アツかった！"))
 
-    print(my_filter.censor("昨日のアーセナルの試合アツかった！"))
-    print(my_filter.censor("昨日のリバプールの試合アツかった！"))
+        # NGワードの変更をする
+        my_filter.convert_ng_word()
--- word_filter.py
+++ word_filter.py
@@ -1,13 +1,36 @@
 class WordFilter:
 
-    def __init__(self, ng_word, replace_word="<censored>"):
-        self.ng_word = ng_word
+    def __init__(self, ng_words=[], replace_word="<censored>"):
+        self.ng_words = ng_words
         self.replace_word = replace_word
 
-    def detect(self, texts):
-        return self.ng_word in texts
+    def detect(self, texts, ng_word_counter=0):
+        return self.ng_words[ng_word_counter] in texts
 
-    def censor(self, texts):
-        if self.detect(texts):
-            return texts.replace(self.ng_word, self.replace_word)
+    def censor(self, texts, counter):
+        if self.detect(texts, counter):
+            texts = texts.replace(self.ng_words[counter], self.replace_word)
+
         return texts
+
+    def all_censor(self, texts):
+        for counter in range(len(self.ng_words)):
+            texts = self.censor(texts=texts, counter=counter)
+
+        return texts
+
+
+    def convert_ng_word(self):
+        self.show_ng_words()
+        convert_number = int(input("何番のNGワードを変えますか？ > "))
+        after_word = input("変更後のワードを入力してください > ")
+
+        # 空文字列なら変更しない
+        if after_word != "":
+            self.ng_words[convert_number] = after_word
+
+    def show_ng_words(self):
+        counter = 0
+        for word in self.ng_words:
+            print(f"{counter}: {word}")
+            counter += 1
