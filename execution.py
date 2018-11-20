a5aa9f384623cdacad0fce9af085b73a84574e38
hibikikkk <kyou9712@gmail.com>
2018-11-20 16:20:27 +0900

    NGワードと入れ替える文字列を変更できるようになった！
    入れ替え文字は標準入力で自由に設定できるようにして、もし空文字だった場合はcensorになるように設定した！

--- execution.py
+++ execution.py
@@ -1,7 +1,13 @@
 from word_filter import WordFilter
 
 if __name__ == "__main__":
-    my_filter = WordFilter("アーセナル")
+    input_replace_word = input("置き換える文字列はなんですか？ > ")
+
+    if input_replace_word != "":
+        my_filter = WordFilter("アーセナル", input_replace_word)
+
+    if input_replace_word == "":
+        my_filter = WordFilter("アーセナル")
 
     # print(my_filter.detect("昨日のアーセナルの試合アツかった！"))
     #
--- word_filter.py
+++ word_filter.py
@@ -1,12 +1,13 @@
 class WordFilter:
 
-    def __init__(self, ng_word):
+    def __init__(self, ng_word, replace_word="<censored>"):
         self.ng_word = ng_word
+        self.replace_word = replace_word
 
     def detect(self, texts):
         return self.ng_word in texts
 
     def censor(self, texts):
         if self.detect(texts):
-            return texts.replace(self.ng_word, "<censored>")
+            return texts.replace(self.ng_word, self.replace_word)
         return texts
