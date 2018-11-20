e7d2bda7fd7e5eb842226dde9a7bcca392cf460a
hibikikkk <kyou9712@gmail.com>
2018-11-20 18:08:44 +0900

    一応、完成！！

--- word_filter.py
+++ word_filter.py
@@ -4,30 +4,30 @@ class WordFilter:
         self.ng_words = ng_words
         self.replace_word = replace_word
 
-    def detect(self, texts, ng_word_counter=0):
-        return self.ng_words[ng_word_counter] in texts
+    def detect(self, texts, ng_word_index=0):
+        return self.ng_words[ng_word_index] in texts
 
-    def censor(self, texts, counter):
-        if self.detect(texts, counter):
-            texts = texts.replace(self.ng_words[counter], self.replace_word)
+    def censor(self, texts, ng_word_index):
+        if self.detect(texts, ng_word_index):
+            texts = texts.replace(self.ng_words[ng_word_index], self.replace_word)
 
         return texts
 
     def all_censor(self, texts):
         for counter in range(len(self.ng_words)):
-            texts = self.censor(texts=texts, counter=counter)
+            texts = self.censor(texts=texts, ng_word_index=counter)
 
         return texts
 
 
     def convert_ng_word(self):
         self.show_ng_words()
-        convert_number = int(input("何番のNGワードを変えますか？ > "))
+        convert_index = int(input("何番のNGワードを変えますか？ > "))
         after_word = input("変更後のワードを入力してください > ")
 
         # 空文字列なら変更しない
         if after_word != "":
-            self.ng_words[convert_number] = after_word
+            self.ng_words[convert_index] = after_word
 
     def show_ng_words(self):
         counter = 0
